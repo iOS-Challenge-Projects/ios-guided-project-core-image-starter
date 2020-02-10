93a08d3b8c5f550d8bd4490ff1cbb3dddee793c9
Paul Solt <PaulSolt@gmail.com>
2020-02-10 13:03:10 -0500

    Created filterImage function and explored the filter attributes

--- PhotoFilter/PhotoFilterViewController.swift
+++ PhotoFilter/PhotoFilterViewController.swift
@@ -1,9 +1,12 @@
 import UIKit
 import CoreImage
+import CoreImage.CIFilterBuiltins
 import Photos
 
 class PhotoFilterViewController: UIViewController {
 
+    private var context = CIContext(options: nil)
+    
 	@IBOutlet weak var brightnessSlider: UISlider!
 	@IBOutlet weak var contrastSlider: UISlider!
 	@IBOutlet weak var saturationSlider: UISlider!
@@ -11,9 +14,35 @@ class PhotoFilterViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
+        
+//        let filter = CIFilter.colorControls() // Like a recipe
+//        print(filter)
+//        print(filter.attributes)
 	}
 	
+    func filterImage(_ image: UIImage) -> UIImage? {
+        // UIImage -> CGImage -> CIImage
+        guard let cgImage = image.cgImage else { return nil }
+        
+        let ciImage = CIImage(cgImage: cgImage)
+        
+        let filter = CIFilter.colorControls() // Like a recipe
+
+        filter.inputImage = ciImage
+        filter.brightness = brightnessSlider.value
+        filter.contrast = contrastSlider.value
+        filter.saturation = saturationSlider.value
+        
+        // CIImage -> CGImage -> UIImage
+        guard let outputCIImage = filter.outputImage else { return nil }
+
+        // Rendering the image (actually baking the cookies)
+        guard let outputCGImage = context.createCGImage(outputCIImage,
+                                                        from: CGRect(origin: .zero, size: image.size)) else { return nil }
+        return UIImage(cgImage: outputCGImage)
+    }
+    
+    
 	// MARK: Actions
 	
 	@IBAction func choosePhotoButtonPressed(_ sender: Any) {
