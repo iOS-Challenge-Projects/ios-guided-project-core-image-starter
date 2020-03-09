beb152d0ecba9adbca5a9e98259c1cbf841f9a5b
Paul Solt <PaulSolt@gmail.com>
2020-03-09 13:01:10 -0400

    Starting Color Filter

--- PhotoFilter/PhotoFilterViewController.swift
+++ PhotoFilter/PhotoFilterViewController.swift
@@ -1,19 +1,62 @@
 import UIKit
 import CoreImage
+import CoreImage.CIFilterBuiltins
 import Photos
 
 class PhotoFilterViewController: UIViewController {
 
+    private var originalImage: UIImage?
+    private let context = CIContext(options: nil)
+    
 	@IBOutlet weak var brightnessSlider: UISlider!
 	@IBOutlet weak var contrastSlider: UISlider!
 	@IBOutlet weak var saturationSlider: UISlider!
 	@IBOutlet weak var imageView: UIImageView!
 	
-	override func viewDidLoad() {
-		super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let filter = CIFilter.colorControls()
+        
+        print(filter)
+        print(filter.attributes)
 
-	}
+        // Use our storyboard placeholder image to start
+        originalImage = imageView.image
+    }
 	
+    func filterImage(_ image: UIImage) -> UIImage? {
+        
+        // UIImage > CGImage > CIImage
+        guard let cgImage = image.cgImage else { return nil }
+        
+        let ciImage = CIImage(cgImage: cgImage)
+        
+        let filter = CIFilter.colorControls()
+        filter.inputImage = ciImage
+        filter.brightness = brightnessSlider.value
+        filter.contrast = contrastSlider.value
+        filter.saturation = saturationSlider.value
+        
+        guard let outputCIImage = filter.outputImage else { return nil }
+        
+        // CIImage > CGImage > UIImage
+        
+        // Render the image (apply the filter to the image) i.e.: baking cookies in oven
+        guard let outputCGImage = context.createCGImage(outputCIImage, from: CGRect(origin: CGPoint.zero, size: image.size)) else { return nil }
+        
+        return UIImage(cgImage: outputCGImage)
+    }
+    
+    private func updateImage() {
+        if let originalImage = originalImage {
+            imageView.image = filterImage(originalImage)
+        } else {
+            imageView.image = nil // allows us to clear out the image
+        }
+    }
+    
+    
 	// MARK: Actions
 	
 	@IBAction func choosePhotoButtonPressed(_ sender: Any) {
@@ -28,16 +71,16 @@ class PhotoFilterViewController: UIViewController {
 
 	// MARK: Slider events
 	
-	@IBAction func brightnessChanged(_ sender: UISlider) {
+    @IBAction func brightnessChanged(_ sender: UISlider) {
+        updateImage()
+    }
 
-	}
-	
-	@IBAction func contrastChanged(_ sender: Any) {
+    @IBAction func contrastChanged(_ sender: Any) {
+        updateImage()
+    }
 
-	}
-	
-	@IBAction func saturationChanged(_ sender: Any) {
-
-	}
+    @IBAction func saturationChanged(_ sender: Any) {
+        updateImage()
+    }
 }
 
