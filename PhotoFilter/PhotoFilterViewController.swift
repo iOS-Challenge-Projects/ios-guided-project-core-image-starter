7d0a47b2fb751f7853ec9819a32e162a41fa6a01
fritzgt <tuanis@live.com>
2020-04-20 16:00:53 +0300

    implemented image picker delegate methods

--- PhotoFilter/PhotoFilterViewController.swift
+++ PhotoFilter/PhotoFilterViewController.swift
@@ -5,7 +5,26 @@ import Photos
 
 class PhotoFilterViewController: UIViewController {
 
-    private var originaImage: UIImage? {
+    //MARK: - Properties
+    
+    private var originalImage: UIImage? {
+        didSet{
+            
+            //Resize image down to make the UI smooth
+            //But when we save the image we actually save the original size image
+            guard let originalImage = originalImage else { return }
+            
+            var scaledSize = imageView.bounds.size
+            let scale = UIScreen.main.scale //1x (no iphone) 2x 3x
+            
+            //how many pixel can we fit in the screen
+            scaledSize = CGSize(width: scaledSize.width * scale, height: scaledSize.height * scale)
+            
+            scaledImage = originalImage.imageByScaling(toSize: scaledSize)
+        }
+    }
+    
+    private var scaledImage: UIImage? {
         didSet{
             updateViews()
         }
@@ -13,18 +32,36 @@ class PhotoFilterViewController: UIViewController {
     
     private var context = CIContext(options: nil)
 
+    
+    //MARK: - Outlets
+    
 	@IBOutlet weak var brightnessSlider: UISlider!
 	@IBOutlet weak var contrastSlider: UISlider!
 	@IBOutlet weak var saturationSlider: UISlider!
 	@IBOutlet weak var imageView: UIImageView!
 	
+    //MARK: - View Lifecycle
+    
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
 	}
     
+    
+    //MARK: - Custom methods
+    
+    func presentImagePickerController() {
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            NSLog("Error the photo library is not available.")
+            return
+        }
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+    }
+    
     func updateViews() {
-        if let originaImage = originaImage {
+        if let originaImage = originalImage {
             imageView.image = filterImage(originaImage)
         } else {
             //Could use a place holder image instead of nil
@@ -60,10 +97,7 @@ class PhotoFilterViewController: UIViewController {
         return UIImage(cgImage: outputCGImage)
     }
     
-    
-    
-	
-	// MARK: Actions
+	//MARK: - Actions
 	
 	@IBAction func choosePhotoButtonPressed(_ sender: Any) {
 		// TODO: show the photo picker so we can choose on-device photos
@@ -78,7 +112,7 @@ class PhotoFilterViewController: UIViewController {
 	}
 	
 
-	// MARK: Slider events
+	//MARK: -  Slider events
 	
 	@IBAction func brightnessChanged(_ sender: UISlider) {
         updateViews()
@@ -93,3 +127,26 @@ class PhotoFilterViewController: UIViewController {
 	}
 }
 
+
+//MARK: - UIImagePickerControllerDelegate
+extension PhotoFilterViewController: UIImagePickerControllerDelegate{
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            originalImage = image
+        }
+        
+        picker.dismiss(animated: true)
+    }
+}
+
+
+//MARK: - UINavigationControllerDelegate
+
+extension PhotoFilterViewController: UINavigationControllerDelegate{
+
+}
